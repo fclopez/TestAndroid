28a08719ef7c1104f528e98dff8c730b5de53d85
Camilo López Castellanos <Camilo Lpez Castellanos>
2016-10-16 14:26:02 -0500

    Servicio rest y manejo de JsonArray

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -6,16 +6,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    private Button btnSumar, btnRestar, btnSaludo;
-    private TextView txtContador, txtSaludo;
+    private Button btnSumar, btnRestar, btnSaludo, btnConsulta;
+    private TextView txtContador, txtSaludo, txtConsulta;
     private EditText editText;
     private int contador = 0;
 
-
-    @Override
+  @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //definimos el layout
@@ -23,14 +23,25 @@ public class MainActivity extends Activity implements View.OnClickListener {
         //Obtenemos una referencia a los controles de la interfaz
         txtContador = (TextView)findViewById(R.id.txtContador);
         txtSaludo = (TextView)findViewById(R.id.txtSaludo);
+        txtConsulta = (TextView)findViewById(R.id.txtConsulta);
         editText = (EditText)findViewById(R.id.editText);
         btnSumar = (Button)findViewById(R.id.btnSumar);
         btnRestar = (Button)findViewById(R.id.btnRestar);
         btnSaludo = (Button)findViewById(R.id.btnSaludo);
+        btnConsulta = (Button)findViewById(R.id.btnConsultar);
         //definimos los eventos de los botones
         btnSumar.setOnClickListener(this);
         btnRestar.setOnClickListener(this);
         btnSaludo.setOnClickListener(this);
+        btnConsulta.setOnClickListener(this);
+    }
+    // encapsulamiento
+    public String getTxtCosulta(){
+        return (String) this.txtConsulta.getText();
+    }
+
+    public void setTxtCosulta(String txt){
+        this.txtConsulta.setText( txt );
     }
 
     @Override
@@ -46,8 +57,13 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 break;
             case R.id.btnSaludo:
                 txtSaludo.setText("Hola: "+editText.getText());
+                //notificacion pop-up
+                Toast.makeText(this,"Hola: "+editText.getText(),Toast.LENGTH_LONG ).show();
+                break;
+            case R.id.btnConsultar:
+                //se envia una comunicacion
+                new RegistroSensor(this).execute("http://hometic.apphb.com/api/Registro");
                 break;
         }
     }
-
 }
---
+++ app/src/main/java/com/example/contador/RegistroSensor.java
@@ -0,0 +1,92 @@
+package com.example.contador;
+
+import android.os.AsyncTask;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+/**
+ * Created by Camilo on 15/10/2016.
+ */
+
+public class RegistroSensor extends AsyncTask<String, String, String> {
+
+    /*definicion de los elementos para la conexion*/
+    HttpURLConnection conexion;
+    BufferedReader rd;
+    StringBuffer  result;
+    MainActivity menup;
+
+    /*Este contructor permite la interacción con la clase principal*/
+    public RegistroSensor(MainActivity princ){
+        this.menup = princ;
+    };
+
+    @Override
+    protected String doInBackground(String... str) {
+        try {
+            URL url = new URL(str[0]);
+            conexion = (HttpURLConnection) url.openConnection();
+            conexion.setRequestMethod("GET");
+            conexion.connect();
+            //InputStream is = (conexion.getResponseCode()==201||conexion.getResponseCode()==200)? conexion.getInputStream(): null;
+
+            InputStream is = conexion.getInputStream();
+            rd = new BufferedReader(new InputStreamReader(is));
+            result = new StringBuffer();
+            String line = "";
+            
+            while ((line = rd.readLine()) != null) {
+                result.append(line+'\n');
+            }
+
+            String data = result.toString();
+
+            //JSONObject jsonObj = new JSONObject(new JSONTokener(result.toString()));
+            JSONArray jsonArray = new JSONArray( data );
+            String msg = "";
+
+            for(int i=0;i<=jsonArray.length()-1;i++){
+                JSONObject obj1 = jsonArray.getJSONObject( i );
+                String id = obj1.getString("Id");
+                String fecha = obj1.getString("FechaRegistro");
+                String lectura = obj1.getString("Lectura");
+                String sensorId = obj1.getString("SensorId");
+                msg += "\n IdRegistro: "+id+"\n Fecha: "+fecha+"\n Mensaje: "+lectura+"\n SensorId: "+sensorId+"\n" ;
+            }
+            return msg;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } finally {
+            if(conexion != null){
+                conexion.disconnect();
+            }
+            try {
+                if(rd != null){
+                    rd.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        menup.setTxtCosulta("Consulta: \n"+s);
+    }
+}
