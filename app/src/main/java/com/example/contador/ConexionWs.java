ee11db06e6a71385ced91cccddb645bc9fd26470
flopez <flopez@colvatel.com>
2016-11-02 15:05:45 -0500

    socket funcionando envio y recepcion de mensajes - falla thread

---
+++ app/src/main/java/com/example/contador/ConexionWs.java
@@ -0,0 +1,53 @@
+package com.example.contador;
+
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+import io.socket.engineio.client.Socket;
+import io.socket.emitter.Emitter;
+
+/**
+ * Created by flopez on 01/11/2016.
+ */
+
+public class ConexionWs {
+
+    private Socket ws;
+    private MainActivity principal;
+
+    public ConexionWs(MainActivity pr) {
+        try {
+            this.principal = pr;
+            this.ws = new Socket("http://172.30.60.86:8000");
+            this.ws.on("status", estado());
+            this.ws.open();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /*Eventos Emmit*/
+    public void enviarMensaje(String evento, String msg){
+        this.ws.emit(evento,msg);
+    }
+    /*Eventos Listener*/
+    public Emitter.Listener estado(){
+        Emitter.Listener estado = new Emitter.Listener() {
+            @Override
+            public void call(final Object... args) {
+                principal.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String data = (String)args[0];
+                        principal.setTxtEstadoSocket("Respuesta del servidor: "+ data);
+
+                    }
+                });
+                //new Tarea(principal).execute((String)args[0]);
+            }
+        };
+        return estado;
+    }
+
+}
--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -8,12 +8,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    private Button btnSumar, btnRestar, btnSaludo, btnConsulta;
-    private TextView txtContador, txtSaludo, txtConsulta;
+    private Button btnSumar, btnRestar, btnSaludo, btnConsulta, btnApagado, btnEncendido;
+    private TextView txtContador, txtSaludo, txtConsulta,txtEstado, txtEstadoSocket;
     private EditText editText;
     private int contador = 0;
+    private ConexionWs app;
 
   @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,26 +27,40 @@ public class MainActivity extends Activity implements View.OnClickListener {
         txtContador = (TextView)findViewById(R.id.txtContador);
         txtSaludo = (TextView)findViewById(R.id.txtSaludo);
         txtConsulta = (TextView)findViewById(R.id.txtConsulta);
+        txtEstado = (TextView)findViewById(R.id.txEstado);
+        txtEstadoSocket = (TextView)findViewById(R.id.txEstadoSocket);
         editText = (EditText)findViewById(R.id.editText);
         btnSumar = (Button)findViewById(R.id.btnSumar);
         btnRestar = (Button)findViewById(R.id.btnRestar);
         btnSaludo = (Button)findViewById(R.id.btnSaludo);
         btnConsulta = (Button)findViewById(R.id.btnConsultar);
+        btnApagado = (Button)findViewById(R.id.btnApagado);
+        btnEncendido = (Button)findViewById(R.id.btnEncendido);
         //definimos los eventos de los botones
         btnSumar.setOnClickListener(this);
         btnRestar.setOnClickListener(this);
         btnSaludo.setOnClickListener(this);
         btnConsulta.setOnClickListener(this);
+        btnApagado.setOnClickListener(this);
+        btnEncendido.setOnClickListener(this);
+
+        //Se inicia el socket
+        app = new ConexionWs(MainActivity.this);
     }
     // encapsulamiento
     public String getTxtCosulta(){
         return (String) this.txtConsulta.getText();
     }
-
     public void setTxtCosulta(String txt){
         this.txtConsulta.setText( txt );
     }
+    public void setTxtEstadoSocket(String txt){this.txtEstadoSocket.setText(txt);}
 
+    @Override
+    public void onDestroy(){
+
+    }
+    
     @Override
     public void onClick(View v) {
         switch (v.getId()){
@@ -63,6 +80,14 @@ public class MainActivity extends Activity implements View.OnClickListener {
             case R.id.btnConsultar:
                 //se envia una comunicacion
                 new RegistroSensor(this).execute("http://hometic.apphb.com/api/Registro");
+                break;
+            case R.id.btnApagado:
+                app.enviarMensaje("SensorPIR","false");
+                txtEstado.setText("Apagado");
+                break;
+            case R.id.btnEncendido:
+                app.enviarMensaje("SensorPIR","true");
+                txtEstado.setText("Encendido");
                 break;
         }
     }
